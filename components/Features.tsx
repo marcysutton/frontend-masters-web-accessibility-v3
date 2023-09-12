083075ccc40d07db8ed2bf7b81e0ea2444adbea3
Marcy Sutton <holla@marcysutton.com>
2023-09-12 11:45:42 -0700

    Adjust overall course structure and links

--- components/Features.tsx
+++ components/Features.tsx
@@ -34,27 +34,20 @@ const features = [
 function Features() {
   return (
     <>
-      <div className="grid grid-cols-1 gap-3 my-12">
+      <ol className="list-disc my-12">
         {features.map(({ icon: Icon, ...feature }, i) => (
-          <div
-            className="flex items-center space-x-4"
+          <li
+            className="space-x-4"
             key={feature.name.split(" ").join("-")}
           >
-            <div>
-              <Icon
-                className="block w-8 h-8"
-                style={{ height: 24, width: 24 }}
-                aria-hidden="true"
-              />
-            </div>
             <div>
               <div className="my-0 font-small dark:text-white">
                 {feature.name}
               </div>
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ol>
     </>
   );
 }
