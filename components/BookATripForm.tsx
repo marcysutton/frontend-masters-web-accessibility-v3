ed75414b0dc53fe707269f8c3e630475e846664f
Marcy sutton Todd <holla@marcysutton.com>
2023-12-10 11:25:03 -0800

    Content updates and screen reader exercise

--- components/BookATripForm.tsx
+++ components/BookATripForm.tsx
@@ -1,102 +1,153 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useEffect, useState, useRef } from 'react';
 import styles from './BookATripForm.module.scss';
+import { IconMinus, IconPlus } from './Icons';
 
 const BookATripForm = () => {
-    const [isOneWay, setIsOneWay] = useState(false);
-    const [childrenToggleOn, setChildrenToggle] = useState(false);
-    const [infantCount, updateInfantCount] = useState(0);
-    const [kidCount, updateKidCount] = useState(0);
+	const [isOneWay, setIsOneWay] = useState<boolean>(false);
+	const [childrenToggleHidden, updateHiddenChildrenToggle] = useState<boolean>(true);
+	const [infantCount, updateInfantCount] = useState<number>(0);
+	const [kidCount, updateKidCount] = useState<number>(0);
+	const [totalKids, setTotalKids] = useState<number>(0);
+	const childrenToggleRef = useRef<HTMLDivElement | null>(null);
 
-    const submitHandler = (event: FormEvent) => {
+	const increment = (counter, callback) => {
+		if (counter < 6) {
+			callback(counter + 1);
+		}
+	};
+	const decrement = (counter, callback) => {
+		if (counter > 0) {
+			callback(counter - 1);
+		}
+	};
+	useEffect(() => {
+		setTotalKids(infantCount + kidCount);
+	}, [infantCount, kidCount]);
 
-    }
-    return (
-        <form onSubmit={submitHandler} className={styles.bookATrip}>
-            <fieldset>
-                <legend>Book a Flight</legend>
-                <div className={styles.checkboxGroup}>
-                    <label className={styles.chkLabel}>
-                        <input type="checkbox" onChange={()=>setIsOneWay(!isOneWay)} />
-                        One-Way
-                    </label>
-                    <label className={styles.chkLabel}>
-                        <input type="checkbox" />
-                        Use miles
-                    </label>
-                </div>
-                <div className={styles.formGroup}>
-                    <label className={styles.inputTextlabel}>
-                        From
-                        <input type="text" name="departureCity" />
-                    </label>
-                    <span className={styles.inputTextlabel}>
-                        To
-                        <input type="text" name="arrivalCity" />
-                    </span>
-                </div>
-                <div className={styles.formGroup}>
-                    <label className={styles.inputTextlabel}>
-                        Departure Date
-                        <input type="date" name="departureDate" />
-                    </label>
-                    <span className={styles.inputTextlabel} hidden={isOneWay ? true : false}>
-                        Return Date
-                        <input type="date" name="returnDate" />
-                    </span>
-                </div>
-                <div className={styles.formGroup}>
-                    <label className={styles.inputTextlabel}>
-                        Adults
-                        <select name="adultCount">
-                            <option>0 adults</option>
-                            <option>1 adult</option>
-                            <option>2 adults</option>
-                            <option>3 adults</option>
-                            <option>4 adults</option>
-                            <option>5 adults</option>
-                            <option>6 adults</option>
-                            <option>7 adults</option>
-                        </select>
-                    </label>
-                    <div className={styles.inputTextlabel}>
-                        Children
-                        <div id="childrenCountDropDown" className={styles.childrenDropDownToggle}>
-                            <div className={styles.childrenDropDownToggle} role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" tabIndex={0}>
-                                <span id="numChildren">{infantCount} {infantCount === 1 ? 'child' : 'children'}</span>
-                            </div>
-                            <ul className={styles.dropdownMenu} hidden={childrenToggleOn ? false : true}>
-                                <li className="children-infant">
-                                    <input type="tel" id="childrenCount" name="ChildrenCount" className="form-control children-input" value="0" tabIndex={0} onChange={()=>updateInfantCount(infantCount + 1)} />
-                                    <label htmlFor="childrenCount" style={{verticalAlign:'text-top'}}>Ages 2-17</label>
-                                    <div className="symbol children-dropdown-icon-wrapper">
-                                        <svg id="childrenMinus" className="children-dropdown-icon icon-minus-circle decrement-count" viewBox="0 0 512 512" style={{fill: 'rgb(200, 201, 199)'}}>
-                                            <path d="m384 274l0-36c0-5-2-10-5-13c-4-4-8-6-13-6l-220 0c-5 0-9 2-13 6c-3 3-5 8-5 13l0 36c0 5 2 10 5 13c4 4 8 6 13 6l220 0c5 0 9-2 13-6c3-3 5-8 5-13z m91-18c0 40-9 77-29 110c-20 34-46 60-80 80c-33 20-70 29-110 29c-40 0-77-9-110-29c-34-20-60-46-80-80c-20-33-29-70-29-110c0-40 9-77 29-110c20-34 46-60 80-80c33-20 70-29 110-29c40 0 77 9 110 29c34 20 60 46 80 80c20 33 29 70 29 110z"></path>
-                                        </svg>
-                                        <svg className="children-dropdown-icon icon-plus-circle increment-count" viewBox="0 0 512 512">
-                                            <path d="m384 274l0-36c0-5-2-10-5-13c-4-4-8-6-13-6l-73 0l0-73c0-5-2-9-6-13c-3-3-8-5-13-5l-36 0c-5 0-10 2-13 5c-4 4-6 8-6 13l0 73l-73 0c-5 0-9 2-13 6c-3 3-5 8-5 13l0 36c0 5 2 10 5 13c4 4 8 6 13 6l73 0l0 73c0 5 2 9 6 13c3 3 8 5 13 5l36 0c5 0 10-2 13-5c4-4 6-8 6-13l0-73l73 0c5 0 9-2 13-6c3-3 5-8 5-13z m91-18c0 40-9 77-29 110c-20 34-46 60-80 80c-33 20-70 29-110 29c-40 0-77-9-110-29c-34-20-60-46-80-80c-20-33-29-70-29-110c0-40 9-77 29-110c20-34 46-60 80-80c33-20 70-29 110-29c40 0 77 9 110 29c34 20 60 46 80 80c20 33 29 70 29 110z"></path>
-                                        </svg>
-                                    </div>
-                                </li>
-                                <li className="children-infant">
-                                    <input type="tel" className="form-control children-input" name="InfantCount" id="infantCount" value="0" tabIndex={0} onChange={()=>updateInfantCount(infantCount - 1)} />
-                                    <label htmlFor="infantCount" style={{verticalAlign:'text-top'}}>Under 2 (on lap)</label>
-                                    <div className="symbol children-dropdown-icon-wrapper">
-                                        <svg id="infantMinus" className="children-dropdown-icon icon-minus-circle decrement-count" viewBox="0 0 512 512" style={{fill: 'rgb(200, 201, 199)'}}>
-                                            <path d="m384 274l0-36c0-5-2-10-5-13c-4-4-8-6-13-6l-220 0c-5 0-9 2-13 6c-3 3-5 8-5 13l0 36c0 5 2 10 5 13c4 4 8 6 13 6l220 0c5 0 9-2 13-6c3-3 5-8 5-13z m91-18c0 40-9 77-29 110c-20 34-46 60-80 80c-33 20-70 29-110 29c-40 0-77-9-110-29c-34-20-60-46-80-80c-20-33-29-70-29-110c0-40 9-77 29-110c20-34 46-60 80-80c33-20 70-29 110-29c40 0 77 9 110 29c34 20 60 46 80 80c20 33 29 70 29 110z"></path>
-                                        </svg>
-                                        <svg id="infantPlus" className="children-dropdown-icon icon-plus-circle increment-count" viewBox="0 0 512 512">
-                                            <path d="m384 274l0-36c0-5-2-10-5-13c-4-4-8-6-13-6l-73 0l0-73c0-5-2-9-6-13c-3-3-8-5-13-5l-36 0c-5 0-10 2-13 5c-4 4-6 8-6 13l0 73l-73 0c-5 0-9 2-13 6c-3 3-5 8-5 13l0 36c0 5 2 10 5 13c4 4 8 6 13 6l73 0l0 73c0 5 2 9 6 13c3 3 8 5 13 5l36 0c5 0 10-2 13-5c4-4 6-8 6-13l0-73l73 0c5 0 9-2 13-6c3-3 5-8 5-13z m91-18c0 40-9 77-29 110c-20 34-46 60-80 80c-33 20-70 29-110 29c-40 0-77-9-110-29c-34-20-60-46-80-80c-20-33-29-70-29-110c0-40 9-77 29-110c20-34 46-60 80-80c33-20 70-29 110-29c40 0 77 9 110 29c34 20 60 46 80 80c20 33 29 70 29 110z"></path>
-                                        </svg>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </fieldset>
-        </form>
-    )
+	const submitHandler = (event: FormEvent) => {};
+	const clickOutsideHandler = (event: MouseEvent) => {
+		if (childrenToggleRef.current.contains(event.target as Node)) {
+			return;
+		}
+		updateHiddenChildrenToggle(true);
+	};
+	useEffect(() => {
+		document.addEventListener('mousedown', clickOutsideHandler);
 
-}
+		return function cleanup() {
+			document.removeEventListener('mousedown', clickOutsideHandler);
+		};
+	}, []);
+	return (
+		<form onSubmit={submitHandler} className={styles.bookATrip}>
+			<fieldset>
+				<legend>Book a Flight</legend>
+				<div className={styles.checkboxGroup}>
+					<label className={styles.chkLabel}>
+						<input type="checkbox" onChange={() => setIsOneWay(!isOneWay)} />
+						One-Way
+					</label>
+					<label className={styles.chkLabel}>
+						<input type="checkbox" />
+						Use miles
+					</label>
+				</div>
+				<div className={styles.formGroup}>
+					<label className={styles.inputTextlabel}>
+						From
+						<input type="text" name="departureCity" />
+					</label>
+					<span className={styles.inputTextlabel}>
+						To
+						<input type="text" name="arrivalCity" />
+					</span>
+				</div>
+				<div className={styles.formGroup}>
+					<label className={styles.inputTextlabel}>
+						Departure Date
+						<input type="date" name="departureDate" />
+					</label>
+					<span className={styles.inputTextlabel} hidden={isOneWay ? true : false}>
+						Return Date
+						<input type="date" name="returnDate" />
+					</span>
+				</div>
+				<div className={styles.formGroup}>
+					<label className={styles.inputTextlabel}>
+						Adults
+						<select name="adultCount">
+							<option>0 adults</option>
+							<option>1 adult</option>
+							<option>2 adults</option>
+							<option>3 adults</option>
+							<option>4 adults</option>
+							<option>5 adults</option>
+							<option>6 adults</option>
+							<option>7 adults</option>
+						</select>
+					</label>
+					<div className={styles.inputTextlabel}>
+						Children
+						<div id="childrenCountDropDown" className={styles.childrenDropDownToggle} ref={childrenToggleRef}>
+							<div
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+								tabIndex={0}
+								onClick={() => updateHiddenChildrenToggle(false)}>
+								<span id="numChildren">
+									{totalKids} {totalKids > 1 ? 'children' : 'child'}
+								</span>
+							</div>
+							<ul className={`${styles.dropdownMenu} ${childrenToggleHidden ? '' : styles.active}`}>
+								<li className="children-infant">
+									<input
+										type="tel"
+										id="InfantCount"
+										name="InfantCount"
+										className="form-control children-input"
+										value={infantCount}
+										tabIndex={0}
+										onChange={(event) => updateInfantCount(event.target.value as unknown as number)}
+									/>
+									<label htmlFor="infantCount">Ages 2-17</label>
+									<div className="symbol children-dropdown-icon-wrapper">
+										<button type="button" onClick={() => decrement(infantCount, updateInfantCount)}>
+											<IconMinus />
+										</button>
+										<button type="button" onClick={() => increment(infantCount, updateInfantCount)}>
+											<IconPlus />
+										</button>
+									</div>
+								</li>
+								<li className="children-infant">
+									<input
+										type="tel"
+										className="form-control children-input"
+										name="KidCount"
+										id="KidCount"
+										value={kidCount}
+										tabIndex={0}
+										onChange={() => updateKidCount(kidCount - 1)}
+									/>
+									<label htmlFor="KidCount">Under 2 (on lap)</label>
+									<div className="symbol children-dropdown-icon-wrapper">
+										<button type="button" onClick={() => decrement(kidCount, updateKidCount)}>
+											<IconMinus />
+										</button>
+										<button type="button" onClick={() => increment(kidCount, updateKidCount)}>
+											<IconPlus />
+										</button>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	);
+};
 
 export default BookATripForm;
--- components/ScreenReaderImageGallery.tsx
+++ components/ScreenReaderImageGallery.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 
 const ScreenReaderImageGallery = () => {
     return (
-        <div style={{display: 'flex', flexDirection: 'row', gap: '1em'}}>
-            <figure style={{margin: '1em 0'}}>
-                <img alt="A woman on a mountain bike going over a big rock" src="/exercises/IMG_0920.JPG" />
-                <figcaption>
-                    <p>Mountain biking in Moab, Utah.</p>
-                </figcaption>
-            </figure>
-            <div style={{margin: '1em 0'}}>
-                <img alt="" src="/exercises/IMG_0920.JPG" />
-            </div>
-        </div>
-    )
+			<div className="grid grid-cols-7 grid-row-1 gap-2 grid">
+				<figure className="my-2 col-span-4">
+					<img
+						alt="a dirt path next to a small lake, with Fall colors of red, yellow and green. The sky is blue with wispy high clouds."
+						className="mb-2"
+						src="/exercises/north-cascades.jpg"
+					/>
+					<figcaption>
+						<p className="italic">Make your escape to the North Cascades.</p>
+					</figcaption>
+				</figure>
+				<div className="my-2 col-span-3">
+					<img alt="" src="/exercises/hawaii.jpg" className="object-cover w-full max-h-full mb-2" />
+					<em>Hawaii calls!</em>
+				</div>
+			</div>
+		);
 }
 
 export default ScreenReaderImageGallery;
